f5939fadb8935aa773cbc8357bbbcf44ae0afa71
Anggi Al Ansori <anggialansori.rpl4@gmail.com>
2020-12-23 11:15:08 +0700

    update stories

--- assets/stories.js
+++ assets/stories.js
@@ -1,6 +1,6 @@
 export default [
-  "Bismillah.. pertama-tama izinkan Aa berterimakasih sudah meluangkan waktunya untuk membuka situs ini, hal yang perlu diketahui bahwa tidak semua orang tau situs ini. terkecuali DIRIMU khusus .",
-  "Alhamdulillah. Qadarullah kita bisa mengenal satu sama lain tepat 4 Bulan, mengetahui kesibukan masing-masing serta pernah cerita satu sama lain walau secara virtual dan tidak bertemu secara langsung .",
-  "Terimakasih sudah berkenan mengisi kekosongan ini, mohon maaf penyampaiannya dengan media seperti ini.",
-  "YOU wanna know who I’m in love with? Read the first word again."
+  "Hiji .",
+  "Dua .",
+  "Tilu.",
+  "Opat."
 ];
