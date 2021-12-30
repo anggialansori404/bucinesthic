3ef6b170bde207ec664a296fbd8be6d30bbde4ad
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-12-30 11:17:17 +0700

    update aja

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "Pablo Neruda", quotes: "I love you without knowing how, or when, or from where." },
+    { author: "noone", quotes: "I love you without knowing how, or when, or from where." },
     { author: "noone", quotes: "Kalau aku mencintai karena Allah, cintamu tak akan pernah mati." },
     { author: "noone", quotes: "Orang kuat bukan berarti ia tidak pernah menangis tetapi orang yang terus istiqomah dalam menghadapi setiap ujian dan godaan." },
-    { author: "noone", quotes: "I love you." },
     { author: "noone", quotes: "Aku akan memohon pada Allah SWT untuk berjumpa denganmu dua kali, sekali di dunia ini dan sekali lagi di surga." },
-    { author: "noone", quotes: "I love you." },
-    { author: "noone", quotes: "I'm sorry for loving you." },
+
 ];
--- assets/stories.js
+++ assets/stories.js
@@ -1,6 +1,6 @@
 export default [
   "Bismillah.. pertama-tama izinkan aku berterimakasih sudah meluangkan waktunya untuk membuka situs ini, hal yang perlu diketahui bahwa tidak semua orang tau situs ini. terkecuali DIRIMU khusus .",
-  "Alhamdulillah. Qadarullah kita bisa mengenal satu sama lain , mengetahui kesibukan masing-masing serta pernah cerita satu sama lain walau secara virtual dan tidak bertemu secara langsung .",
-  "Terimakasih sudah berkenan mengisi kekosongan ini, mohon maaf penyampaiannya dengan media seperti ini.",
+  "Alhamdulillah. Qadarullah kita bisa mengenal satu sama lain , mengetahui kesibukan masing-masing serta pernah cerita satu sama lain walau secara virtual dan Alhamdulillah bertemu secara langsung .",
+  "Terimakasih sudah berkenan menjadi pendamping.",
   "YOU wanna know who I’m in love with? Read the first word again."
 ];
